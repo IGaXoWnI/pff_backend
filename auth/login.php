5c10c4dbd3f654563554cd9380404ff0dca57840
ISMAIL <igaxowni@gmail.com>
2023-06-22 03:06:08 +0100

    add getData function and edit login.php file

--- auth/login.php
+++ auth/login.php
@@ -4,12 +4,12 @@ include "../connect.php";
 
 $password = sha1($_POST['password']);
 $email = filterRequest("email") ;
-$stmt = $con->prepare("SELECT * FROM users WHERE users_email = ? AND users_password = ? ");
-$stmt->execute(array($email,$password));
-$count = $stmt->rowCount();
-result($count) ;
+// $stmt = $con->prepare("SELECT * FROM users WHERE users_email = ? AND users_password = ? ");
+// $stmt->execute(array($email,$password));
+// $count = $stmt->rowCount();
+// result($count) ;
 
-
+getData("users" , "users_email = ? AND users_password = ?" , array($email , $password));
 
 
 
